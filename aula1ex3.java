67299474d1f8f9938a9f07ce9c01fc7e077f0ed8
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-04-08 13:49:35 -0300

    aula1ex3

---
+++ aula1ex3.java
@@ -0,0 +1,13 @@
+package com.mycompany.aula1ex2;
+
+public class aula1ex3 
+{
+    public static void main(String[] args) 
+    {
+    int celsius = 10, farenheit = 0;
+    
+    farenheit = (180 * celsius/100) + 32;
+    
+    System.out.println("Resultado: " +farenheit);
+    }
+}
