aed0a00a41c626ab04a4f320f41d2bc0dac9a09d
Paulo Rabelo <pauloeduardorabelomiranda@gmail.com>
2024-04-10 10:21:11 -0300

    aula1ex3.java

--- aula1ex3.java
+++ aula1ex3.java
@@ -4,10 +4,10 @@ public class aula1ex3
 {
     public static void main(String[] args) 
     {
-    int celsius = 10, farenheit = 0;
+        int celsius = 10, farenheit = 0;
     
-    farenheit = (180 * celsius/100) + 32;
+        farenheit = (180 * celsius/100) + 32;
     
-    System.out.println("Resultado: " +farenheit);
+        System.out.println("Resultado: " +farenheit);
     }
 }
